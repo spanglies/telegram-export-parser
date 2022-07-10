8e4c8826654b1ae96a25dc2d3c2bfe604430132c
Spanglies <eedqsa@gmail.com>
2022-07-09 18:32:50 -0700

    Added better defaults Added directory parsing cleaned up comments cleaned up error message Added ability to output only messages without names

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import argparse
 from datetime import datetime
 import json
 from bs4 import BeautifulSoup
-
+import glob
 
 class TelegramMessage:
     """Class wrapping a telegram message"""
@@ -18,7 +18,7 @@ class TelegramMessage:
         return "["+self.date.isoformat()+"] "+self.sender +": "+self.message
 
     def to_object(self):
-        """ Do fucking shit"""
+        """ return a dict"""
         return {"date": self.date.isoformat(),
                 "sender": self.sender,
                 "message": self.message}
@@ -31,14 +31,17 @@ class TelegramMessage:
         """ return in html format"""
         return "<b>"+self.sender +"</b>: "+ self.message
 
+    def only_message(self):
+        return self.message
+
+
 def main():
     """ main function of script """
-    parser = argparse.ArgumentParser(description=
-                                     """python script to parse telegram
-                                     html chat logs exported by the desktop client""")
-    parser.add_argument("-i", "--input", nargs=1, required=True, type=argparse.FileType('r'))
-    parser.add_argument("-o", "--output", nargs=1, type=argparse.FileType('a'))
-    parser.add_argument("-f", "--format", nargs=1, choices=["text", "json", "plain", "html"])
+    parser = argparse.ArgumentParser(description="python script to parse telegram html chat"
+                                                 " logs exported by the desktop client")
+    parser.add_argument("-i", "--input", nargs=1, required=True)
+    parser.add_argument("-o", "--output", nargs=1, default="./output", type=argparse.FileType('a'))
+    parser.add_argument("-f", "--format", nargs=1, choices=["text", "json", "plain", "html", "message"])
     parser.add_argument("-v", "--verbose", action='store_true')
     parser.add_argument("-r", "--replace", nargs="*")
 
@@ -47,52 +50,56 @@ def main():
 
     if parsed.replace is not None:
         if len(parsed.replace)%2 != 0:
-            print("If you are using the replace tool, please use an even number, you fucking brick")
+            print("If you are using the replace tool, please use an even number")
             return
 
-    soup = BeautifulSoup(parsed.input[0], "html.parser")
-
-
-    alldefaultmesssages = soup.select(".message.default")
-
     objects = []
-    last_name = ""
-    for mess in alldefaultmesssages:
-        textselection = mess.select_one(".text")
-        dateselection = mess.select_one(".date")
-        date = datetime.strptime(dateselection["title"], "%d.%m.%Y %H:%M:%S")
+    for filename in glob.glob(parsed.input[0]):
+        with open(filename, "r") as file:
+            soup = BeautifulSoup(file, "html.parser")
+            alldefaultmesssages = soup.select(".message.default")
 
-        name = ""
-        if "joined" in mess["class"]:
-            name = last_name
-        else:
-            name = mess.select_one(".from_name").text.strip()
-            last_name = name
+            last_name = ""
+            for mess in alldefaultmesssages:
+                textselection = mess.select_one(".text")
+                dateselection = mess.select_one(".date")
+                date = datetime.strptime(dateselection["title"], "%d.%m.%Y %H:%M:%S")
 
-        if parsed.replace is not None:
-            if name in parsed.replace[::2]:
-                name = parsed.replace[parsed.replace.index(name)+1]
-                last_name = name
+                if "joined" in mess["class"]:
+                    name = last_name
+                else:
+                    name = mess.select_one(".from_name").text.strip()
+                    last_name = name
 
+                if parsed.replace is not None:
+                    if name in parsed.replace[::2]:
+                        name = parsed.replace[parsed.replace.index(name)+1]
+                        last_name = name
 
-        if textselection is not None:
-            obj = TelegramMessage(date, name, textselection.text.strip())
-            objects.append(obj)
-            if parsed.verbose is True:
-                print(obj.to_text_string())
-    
-    if parsed.format[0] == "json":
-        json.dump([x.to_object() for x in objects], parsed.output[0])
+                if textselection is not None:
+                    obj = TelegramMessage(date, name, textselection.text.strip())
+                    objects.append(obj)
+                    if parsed.verbose is True:
+                        print(obj.to_text_string())
+    if isinstance(parsed.output, list):
+        outfile = parsed.output[0]
+    else:
+        outfile = parsed.output
+
+    if parsed.format is None or parsed.format[0] == "json":
+        json.dump([x.to_object() for x in objects], outfile)
     elif parsed.format[0] == "text":
         for exs in objects:
-            parsed.output[0].write(exs.to_text_string()+"\n")
+            outfile.write(exs.to_text_string()+"\n")
+    elif parsed.format[0] == "message":
+        for exs in objects:
+            outfile.write(exs.only_message()+"\n")
     elif parsed.format[0] == "plain":
         for exs in objects:
-            parsed.output[0].write(exs.to_plain_string()+"\n")
+            outfile.write(exs.to_plain_string()+"\n")
     elif parsed.format[0] == "html":
         for exs in objects:
-            parsed.output[0].write(exs.to_html()+"\n")
-
+            outfile.write(exs.to_html()+"\n")
 
 
 if __name__ == "__main__":
